8e25dca56e9134bce37f7541cb51525078e2daf7
dwiprst13 <dwiprst13@gmail.com>
2024-02-18 13:43:22 +0700

    s

--- src/component/parts/IconHeader.jsx
+++ src/component/parts/IconHeader.jsx
@@ -4,7 +4,7 @@ function IconHeader() {
   return (
     <button className="grid grid-cols-6 items-center gap-3 md:flex md:flex-row text-white text-xl lg:text-[2rem]">
       <img
-        src="src/assets/himatik.png"
+        src="../src/assets/himatik.png"
         alt=""
         className="h-10 col-span-2 md:flex items-center "
       />
